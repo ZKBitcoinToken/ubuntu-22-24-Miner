e3f6ce8768208ea331e41d0b94ac0e3ffaac97b8
zkBitcoin Token <128942687+ZKBitcoinToken@users.noreply.github.com>
2024-07-05 02:52:49 -0500

    Update 1zkBTCMiner.sh

--- zkBTC_miner_ubuntu_22_&_24/1zkBTCMiner.sh
+++ zkBTC_miner_ubuntu_22_&_24/1zkBTCMiner.sh
@@ -8,6 +8,10 @@ then
     echo "Installing dotnet 5.0..."
     ubuntu_version=$(lsb_release -rs)
     if [[ "$ubuntu_version" == "22.04" || "$ubuntu_version" == "24.04" ]]; then
+        wget 'http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb'
+        
+        sudo dpkg -i libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb
+
         target_version="20.04"
     else
         target_version="$ubuntu_version"
