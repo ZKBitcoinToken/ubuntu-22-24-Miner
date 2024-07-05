d36cd70ed7cb0c54c8f9df42711ad922b08c9a90
zkBitcoin Token <128942687+ZKBitcoinToken@users.noreply.github.com>
2024-07-05 03:00:41 -0500

    Update 1zkBTCMiner.sh

--- zkBTC_miner_ubuntu_22_&_24/1zkBTCMiner.sh
+++ zkBTC_miner_ubuntu_22_&_24/1zkBTCMiner.sh
@@ -7,7 +7,7 @@ then
     echo "dotnet 5.0 is not found or not installed."
     echo "Installing dotnet 5.0..."
     ubuntu_version=$(lsb_release -rs)
-    if [[ "$ubuntu_version" == "22.04" || "$ubuntu_version" == "24.04" ]]; then
+    if [[ "$ubuntu_version" == "22.04" || "$ubuntu_version" == "24.04" || "$ubuntu_version" == "20.04" ]]; then
         wget 'http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb'
         
         sudo dpkg -i libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb
