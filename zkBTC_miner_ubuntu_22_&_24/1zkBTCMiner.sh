bd62ddc6869e3ff4e98cc31fcf204e0a00821586
zkBitcoin Token <128942687+ZKBitcoinToken@users.noreply.github.com>
2024-07-05 04:26:26 -0500

    Update 1zkBTCMiner.sh

--- zkBTC_miner_ubuntu_22_&_24/1zkBTCMiner.sh
+++ zkBTC_miner_ubuntu_22_&_24/1zkBTCMiner.sh
@@ -36,7 +36,7 @@ else
 fi
 
 while : ; do
-  output=$(dotnet _zkBitcoinMiner.dll allowCPU=false allowIntel=true allowAMD=true pool=http://pool.zkbitcoin.org:6122 minerAddress=0xF8B99643fAfC79d9404DE68E48C4D49a3936f78 allowCUDA=true web3api=https://mainnet.era.zksync.io abiFile=zkBTC.abi contract=0x366d17aDB24A7654DbE82e79F85F9Cb03c03cD0D MaxZKBTCperMint=200 MinZKBTCperMint=150 HowManyBlocksAWAYFromAdjustmentToSendMinimumZKBTC=500 gasToMine=0.35 gasApiMax=1 gasLimit=1000000 gasApiURL= gasApiPath=$.safeLow gasApiMultiplier=0.1 gasApiOffset=1.0 2>&1 | tee /dev/tty)
+  output=$(dotnet _zkBitcoinMiner.dll allowCPU=false allowIntel=true allowAMD=true pool=http://pool.zkbitcoin.org:6122 minerAddress=0xF8B99643fAfC79d9404DE68E48C4D49a3936f78 allowCUDA=true web3api=https://mainnet.era.zksync.io abiFile=zkBTC.abi contract=0x366d17aDB24A7654DbE82e79F85F9Cb03c03cD0D MaxZKBTCperMint=50 MinZKBTCperMint=50 HowManyBlocksAWAYFromAdjustmentToSendMinimumZKBTC=500 gasToMine=0.35 gasApiMax=1 gasLimit=1000000 gasApiURL= gasApiPath=$.safeLow gasApiMultiplier=0.1 gasApiOffset=1.0 2>&1 | tee /dev/tty)
   [[ $? -eq 22 ]] || break
 done
 
